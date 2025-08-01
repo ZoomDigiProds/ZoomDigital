dcdd3895b5cad1a81882e1b19ff4f9fff009009d
TusharHirawat <tusharhirawat1918@gmail.com>
2025-08-01 16:36:53 +0100

    updated styling for all views

--- InternalProj/Controllers/ViewWorkController.cs
+++ InternalProj/Controllers/ViewWorkController.cs
@@ -16,6 +16,85 @@ namespace InternalProj.Controllers
             _context = context;
         }
 
+        //[HttpGet]
+        //public IActionResult Index(string studio, DateTime? fromDate, DateTime? toDate, int? workTypeId, int currentPage = 1, bool isSearch = false)
+        //{
+        //    const int pageSize = 10;
+
+        //    bool hasAnyFilter = !string.IsNullOrEmpty(studio) || fromDate.HasValue || toDate.HasValue || (workTypeId.HasValue && workTypeId.Value > 0);
+
+        //    // Only load results if user clicked search
+        //    bool shouldShowResults = isSearch || hasAnyFilter;
+
+        //    var query = _context.WorkOrders
+        //        .Include(w => w.Customer)
+        //        .Include(w => w.WorkType)
+        //        .Where(w => w.Active == "Y");
+
+        //    if (shouldShowResults)
+        //    {
+        //        if (!string.IsNullOrWhiteSpace(studio))
+        //            query = query.Where(w => w.Customer.StudioName.Trim().ToLower() == studio.Trim().ToLower());
+
+        //        if (fromDate.HasValue)
+        //            query = query.Where(w => w.Wdate >= DateTime.SpecifyKind(fromDate.Value.Date, DateTimeKind.Utc));
+
+        //        if (toDate.HasValue)
+        //            query = query.Where(w => w.Wdate <= DateTime.SpecifyKind(toDate.Value.Date.AddDays(1).AddTicks(-1), DateTimeKind.Utc));
+
+        //        if (workTypeId.HasValue && workTypeId.Value > 0)
+        //            query = query.Where(w => w.WorkTypeId == workTypeId.Value);
+        //    }
+        //    else
+        //    {
+        //        // Don't return anything
+        //        query = query.Where(w => false);
+        //    }
+
+        //    var totalItems = query.Count();
+        //    var totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+        //    var viewModel = new WorkOrderViewModel
+        //    {
+        //        StudioList = _context.CustomerRegs.Select(c => c.StudioName).Distinct().ToList(),
+        //        WorkTypes = _context.WorkTypes.ToList(),
+        //        ResultsSummary = query
+        //            .OrderBy(w => w.Wdate)
+        //            .Skip((currentPage - 1) * pageSize)
+        //            .Take(pageSize)
+        //            .ToList()
+        //            .Select(w =>
+        //            {
+        //                var totalPaid = _context.Receipts
+        //                    .Where(r => r.WorkOrderId == w.WorkOrderId)
+        //                    .Sum(r => (double?)r.CurrentAmount) ?? 0;
+
+        //                return new WorkOrderSummaryViewModel
+        //                {
+        //                    WorkOrderId = w.WorkOrderId,
+        //                    WorkOrderNo = w.WorkOrderNo,
+        //                    StudioName = w.Customer?.StudioName,
+        //                    Advance = w.Advance ?? 0,
+        //                    SubTotal = w.SubTotal,
+        //                    TotalPaid = totalPaid,
+        //                    Balance = Math.Max(0, w.SubTotal - (w.Advance ?? 0) - totalPaid),
+        //                    WorkTypeName = w.WorkType?.TypeName,
+        //                    Wdate = w.Wdate
+        //                };
+        //            }).ToList(),
+
+        //        CurrentPage = currentPage,
+        //        TotalPages = totalPages,
+        //        StudioFilter = studio,
+        //        FromDateFilter = fromDate,
+        //        ToDateFilter = toDate,
+        //        WorkTypeFilter = workTypeId
+        //    };
+
+        //    return View(viewModel);
+        //}
+
+
         [HttpGet]
         public IActionResult Index(string studio, DateTime? fromDate, DateTime? toDate, int? workTypeId, int currentPage = 1, bool isSearch = false)
         {
@@ -23,7 +102,6 @@ namespace InternalProj.Controllers
 
             bool hasAnyFilter = !string.IsNullOrEmpty(studio) || fromDate.HasValue || toDate.HasValue || (workTypeId.HasValue && workTypeId.Value > 0);
 
-            // Only load results if user clicked search
             bool shouldShowResults = isSearch || hasAnyFilter;
 
             var query = _context.WorkOrders
@@ -47,7 +125,6 @@ namespace InternalProj.Controllers
             }
             else
             {
-                // Don't return anything
                 query = query.Where(w => false);
             }
 
@@ -91,8 +168,14 @@ namespace InternalProj.Controllers
                 WorkTypeFilter = workTypeId
             };
 
+            if (Request.Headers["X-Requested-With"] == "XMLHttpRequest")
+            {
+                return PartialView("_WorkOrderResultsPartial", viewModel);
+            }
+
             return View(viewModel);
         }
+
 
 
         [HttpPost]
